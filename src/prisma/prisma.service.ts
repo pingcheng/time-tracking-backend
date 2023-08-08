33721b706bb61c4d4cb62fcf8975139eefec51f4
Ping Cheng <neroc7@gmail.com>
2023-08-08 21:36:57 +1000

    log query

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,36 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaClientOptions } from '@prisma/client/runtime/library';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<PrismaClientOptions, 'query'>
+  implements OnModuleInit
+{
+  private readonly logger: Logger;
+
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    });
+    this.logger = new Logger(PrismaService.name);
+  }
+
   async onModuleInit() {
+    this.$on('query', (e) => {
+      this.logger.log(
+        JSON.stringify({
+          Query: e.query,
+          Params: e.params,
+          Duration: e.duration,
+        }),
+      );
+    });
     await this.$connect();
   }
 }
