7968c02c5ba185e328153304ad9a7bc284fb2a1d
Ping Cheng <neroc7@gmail.com>
2023-08-13 22:58:07 +1000

    fix test

--- src/project/project.controller.spec.ts
+++ src/project/project.controller.spec.ts
@@ -2,13 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectController } from './project.controller';
 import { ConfigModule } from '@nestjs/config';
 import { ProjectModule } from './project.module';
+import { TaskModule } from '../task/task.module';
 
 describe('ProjectController', () => {
   let controller: ProjectController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ProjectModule, ConfigModule.forRoot({ isGlobal: true })],
+      imports: [
+        ProjectModule,
+        TaskModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+      ],
       controllers: [ProjectController],
     }).compile();
 
